47bcd3e6773de4f365a2e1f53f622ca06736600d
Egan Lau <eganlau@hotmail.com>
2019-10-08 01:59:14 +0800

    first Starlette server script

--- src/run_v3.py
+++ src/run_v3.py
@@ -1,21 +1,9 @@
-# To add a new cell, type '#%%'
-# To add a new markdown cell, type '#%% [markdown]'
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
-# ms-python.python added
-import os
-try:
-	os.chdir(os.path.join(os.getcwd(), 'src'))
-	print(os.getcwd())
-except:
-	pass
-#%% [markdown]
-# ### FT Data
+# Execute it like:
+# http://localhost:3001/ingredients?filename=1.jpg
 
-#%%
-import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn
-import numpy as np
+# import torch.nn as nn
+# import numpy as np
 import os
 from args import get_parser
 import pickle
@@ -23,76 +11,49 @@ from model import get_model
 from torchvision import transforms
 from utils.output_utils import prepare_output, get_ingrs
 from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 import time
 import pandas as pd
+# import requests
+# from io import BytesIO
+import random
+# from collections import Counter
 
-#%% [markdown]
-# Set ```data_dir``` to the path including vocabularies and model checkpoint
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse
+import torch
+from pathlib import Path
+from io import BytesIO
+import sys
+import uvicorn
+import asyncio
 
-#%%
+args = get_parser()
+# args.maxseqlen = 15
+args.ingrs_only=True
+args.image_model='resnet152'
+
 data_dir = '../data'
 
-
-#%%
 # code will run in gpu if available and if the flag is set to True, else it will run on cpu
 use_gpu = True
 device = torch.device('cuda' if torch.cuda.is_available() and use_gpu else 'cpu')
 map_loc = None if torch.cuda.is_available() and use_gpu else 'cpu'
 
 
-#%%
-torch.cuda.is_available()
-
-
-#%%
-# code below was used to save vocab files so that they can be loaded without Vocabulary class
-#ingrs_vocab = pickle.load(open(os.path.join(data_dir, 'final_recipe1m_vocab_ingrs.pkl'), 'rb'))
-#ingrs_vocab = [min(w, key=len) if not isinstance(w, str) else w for w in ingrs_vocab.idx2word.values()]
-#vocab = pickle.load(open(os.path.join(data_dir, 'final_recipe1m_vocab_toks.pkl'), 'rb')).idx2word
-#pickle.dump(ingrs_vocab, open('../demo/ingr_vocab.pkl', 'wb'))
-#pickle.dump(vocab, open('../demo/instr_vocab.pkl', 'wb'))
-
 ingrs_vocab = pickle.load(open(os.path.join(data_dir, 'fittime_ingr_vocab.pkl'), 'rb'))
 vocab = pickle.load(open(os.path.join(data_dir, 'fittime_instr_vocab.pkl'), 'rb'))
+dataset = pickle.load(open(os.path.join(data_dir, 'train_dataset.pkl'), 'rb'))
 
 ingr_vocab_size = len(ingrs_vocab)
 instrs_vocab_size = len(vocab)
-output_dim = instrs_vocab_size
 
-
-#%%
-dataset = pickle.load(open(os.path.join(data_dir, 'train_dataset.pkl'), 'rb'))
-
-
-#%%
-dataset
-
-
-#%%
 transf_list = []
 transf_list.append(transforms.Resize(256))
 transf_list.append(transforms.CenterCrop(224))
 transform = transforms.Compose(transf_list)
 
-
-
-
-#%%
-print (instrs_vocab_size, ingr_vocab_size)
-
-
-#%%
-ingr_df = pd.DataFrame(ingrs_vocab)
-ingr_df.head()
-
-
-#%%
 t = time.time()
-import sys; sys.argv=['']; del sys
-args = get_parser()
-# args.maxseqlen = 15
-args.ingrs_only=True
-args.image_model='resnet152'
 # args.embed_size=80
 model = get_model(args, ingr_vocab_size, instrs_vocab_size)
 # Load the trained model parameters
@@ -105,55 +66,32 @@ model.recipe_only = False
 print ('loaded model')
 print ("Elapsed time:", time.time() -t)
 
-
-#%%
 transf_list_batch = []
 transf_list_batch.append(transforms.ToTensor())
 transf_list_batch.append(transforms.Normalize((0.485, 0.456, 0.406), 
                                               (0.229, 0.224, 0.225)))
 to_input_transf = transforms.Compose(transf_list_batch)
 
-
-#%%
 greedy = [True, False, False, False]
 beam = [-1, -1, -1, -1]
 temperature = 1.0
 numgens = len(greedy)
 
-#%% [markdown]
-# Set ```use_urls = True``` to get recipes for images in ```demo_urls```. 
-# 
-# You can also set ```use_urls = False``` and get recipes for images in the path in ```data_dir/test_imgs```.
-
-#%%
-import requests
-from io import BytesIO
-import random
-from collections import Counter
-use_urls = False # set to true to load images from demo_urls instead of those in test_imgs folder
 show_anyways = False #if True, it will show the recipe even if it's not valid
 image_folder = os.path.join(data_dir, 'demo_imgs/fittime')
 
-if not use_urls:
-    demo_imgs = os.listdir(image_folder)
-    random.shuffle(demo_imgs)
+demo_files = os.listdir(image_folder)
 
-demo_urls = ['https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/12/9/0/FNK_Cheesecake_s4x3.jpg.rend.hgtvcom.826.620.suffix/1387411272847.jpeg',
-            'https://www.196flavors.com/wp-content/uploads/2014/10/california-roll-3-FP.jpg']
+print("before app")
+app = Starlette()
 
-demo_files = demo_urls if use_urls else demo_imgs
+@app.route("/ingredients", methods=["GET"])
+async def classify_url(request):
 
-
-#%%
-for img_file in demo_files:
-    
-    if use_urls:
-        response = requests.get(img_file)
-        image = Image.open(BytesIO(response.content))
-    else:
-        image_path = os.path.join(image_folder, img_file)
-        image = Image.open(image_path).convert('RGB')
-    
+    image_path = os.path.join(image_folder, request.query_params["filename"])
+    if not os.path.exists(image_path):
+        return JSONResponse({"error": "file doesn't exist"})
+    image = Image.open(image_path).convert('RGB')
     transf_list = []
     transf_list.append(transforms.Resize(256))
     transf_list.append(transforms.CenterCrop(224))
@@ -161,19 +99,20 @@ for img_file in demo_files:
     
     image_transf = transform(image)
     image_tensor = to_input_transf(image_transf).unsqueeze(0).to(device)
-
-    # plt.imshow(image_transf)
-    # plt.axis('off')
-    # plt.show()
-    # plt.close()
-    
     num_valid = 1
+    result = []
     for i in range(numgens):
         with torch.no_grad():
             outputs = model.sample(image_tensor, greedy=greedy[i], 
                                    temperature=temperature, beam=beam[i], true_ingrs=None)
             
         ingr_ids = outputs['ingr_ids'].cpu().numpy()
-        print(ingr_ids[0])
-        print(get_ingrs(ingr_ids[0], ingrs_vocab))
+        result.append({"ingredients_id":pd.Series(ingr_ids[0]).to_json(orient='values'),
+                        "ingredients_name":get_ingrs(ingr_ids[0], ingrs_vocab)})
+    print(result)
 
+    return JSONResponse(result)
+ 
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=3001)
